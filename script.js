8903c78e191b4ba676b278722c478f69a573d126
Everton Cordeiro <99029999+EvertonCordeiro1994@users.noreply.github.com>
2024-07-11 16:19:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-// const hambuguer = document.querySelector("#iconHambuguer")
+const btnCoupe = document.querySelector("#btn-coupe");
+const btnCabriolet = document.querySelector("#btn-cabriolet");
+const carroHome = document.querySelector("#carro-home");
+const texto02 = document.querySelector("#texto02");
 
+btnCoupe.addEventListener('click', function () {
+    btnCabriolet.classList.remove('btn-ativado-cabriolet');
+    btnCoupe.classList.add('btn-ativado-coupe');
+    carroHome.src = "https://images-porsche.imgix.net/-/media/646ED7CDD4DF4060A4823F3A9DB8DA22_97CB2E119D8749C19004EC939CD09E96_CZ25W01IX0010911-carrera-side?w=1400&q=85&crop=faces%2Centropy%2Cedges&auto=format";
+    texto02.style.color = "#4464A7";
+});
 
-// hambuguer.addEventListener("click", function() {
-
-
-//     <i class="fa-solid fa-xmark"></i>
-
-// })
+btnCabriolet.addEventListener('click', function () {
+    btnCoupe.classList.remove('btn-ativado-coupe');
+    btnCabriolet.classList.add('btn-ativado-cabriolet');
+    carroHome.src = "https://images-porsche.imgix.net/-/media/29200583E46C486581905B15B7F99E2A_C9D24F865FEE489D848D0D5D1A3FB656_CZ25W02IX0010-911-carrera-cabrio-side?w=1400&q=85&crop=faces%2Centropy%2Cedges&auto=format";
+    texto02.style.color = "#A5A64A";
+});
